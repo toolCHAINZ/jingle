a5c585bf1d00df61001e0b7269b37b68214e89f9
Mark <mark.denhoed@cs.ox.ac.uk>
2024-06-27 17:05:12 +0100

    Try section flags instead of segment flags

--- jingle_sleigh/src/context/builder/image/gimli.rs
+++ jingle_sleigh/src/context/builder/image/gimli.rs
@@ -2,15 +2,15 @@ use crate::context::builder::image::Perms;
 use crate::context::{Image, ImageSection};
 use crate::JingleSleighError;
 use crate::JingleSleighError::ImageLoadError;
-use object::elf::{PF_R, PF_W, PF_X};
+use object::elf::{PF_R, PF_W, PF_X, SHF_EXECINSTR, SHF_WRITE};
 use object::macho::{VM_PROT_EXECUTE, VM_PROT_READ, VM_PROT_WRITE};
-use object::{Architecture, Endianness, File, Object, ObjectSegment, SegmentFlags};
+use object::{Architecture, Endianness, File, Object, ObjectSection, ObjectSegment, SectionFlags, SegmentFlags};
 
 impl<'d> TryFrom<File<'d>> for Image {
     type Error = JingleSleighError;
     fn try_from(value: File) -> Result<Self, Self::Error> {
         let mut img: Image = Image { sections: vec![] };
-        for x in value.segments() {
+        for x in value.sections() {
             let base_address = x.address();
             let data = x.data().map_err(|_| ImageLoadError)?.to_vec();
             let perms = map_flags(&x.flags());
@@ -24,14 +24,14 @@ impl<'d> TryFrom<File<'d>> for Image {
     }
 }
 
-fn map_flags(flags: &SegmentFlags) -> Perms {
+fn map_flags(flags: &SectionFlags) -> Perms {
     match flags {
-        SegmentFlags::Elf { p_flags } => Perms {
-            exec: (p_flags & PF_X) == PF_X,
-            write: (p_flags & PF_W) == PF_W,
-            read: (p_flags & PF_R) == PF_R,
+        SectionFlags::Elf { sh_flags } => Perms {
+            exec: (*sh_flags as u32 & SHF_EXECINSTR) == SHF_EXECINSTR,
+            write: (*sh_flags as u32 & SHF_WRITE) == SHF_WRITE,
+            read: true,
         },
-        SegmentFlags::MachO { flags, .. } => Perms {
+        SectionFlags::MachO { flags, .. } => Perms {
             exec: (flags & VM_PROT_EXECUTE) == VM_PROT_EXECUTE,
             write: (flags & VM_PROT_WRITE) == VM_PROT_WRITE,
             read: (flags & VM_PROT_READ) == VM_PROT_READ,
