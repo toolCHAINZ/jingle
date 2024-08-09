2e329908e22c284a2ae45180ed7911c93aa18580
Mark <mark.denhoed@cs.ox.ac.uk>
2024-08-09 14:55:20 +0100

    cargo fmt

--- jingle/src/main.rs
+++ jingle/src/main.rs
@@ -1,33 +1,27 @@
-use std::path::PathBuf;
 use clap::builder::Str;
 use clap::{Parser, Subcommand};
-use serde::{Deserialize, Serialize};
-use tracing::Level;
 use jingle_sleigh::context::SleighContextBuilder;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use tracing::Level;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct JingleConfig {
     ghidra_path: PathBuf,
 }
 
-impl Default for JingleConfig{
+impl Default for JingleConfig {
     fn default() -> Self {
         return if cfg!(target_os = "windows") {
             let path = PathBuf::from(r"C:\Program Files\ghidra");
-            Self {
-                ghidra_path: path
-            }
+            Self { ghidra_path: path }
         } else if cfg!(target_os = "macos") {
             let path = PathBuf::from(r"/Applications/ghidra");
-            Self {
-                ghidra_path: path
-            }
+            Self { ghidra_path: path }
         } else {
             let path = PathBuf::from(r"/opt/ghidra");
-            Self {
-                ghidra_path: path
-            }
-        }
+            Self { ghidra_path: path }
+        };
     }
 }
 
@@ -36,7 +30,7 @@ impl Default for JingleConfig{
 struct JingleParams {
     #[command(subcommand)]
     command: Commands,
-    ghidra_path: Option<String>
+    ghidra_path: Option<String>,
 }
 
 #[derive(Debug, Subcommand)]
@@ -54,7 +48,7 @@ enum Commands {
         architecture: String,
         hex_bytes: String,
     },
-    Architectures
+    Architectures,
 }
 
 fn main() {
@@ -67,24 +61,23 @@ fn main() {
         Commands::Disassemble { .. } => {}
         Commands::Lift { .. } => {}
         Commands::Model { .. } => {}
-        Commands::Architectures => list_architectures(config.ghidra_path)
+        Commands::Architectures => list_architectures(config.ghidra_path),
     }
 
-
-fn list_architectures(ghidra: PathBuf) {
-    let sleigh = SleighContextBuilder::load_ghidra_installation(ghidra).unwrap();
-    for language_id in sleigh.get_language_ids() {
-        println!("{}", language_id)
+    fn list_architectures(ghidra: PathBuf) {
+        let sleigh = SleighContextBuilder::load_ghidra_installation(ghidra).unwrap();
+        for language_id in sleigh.get_language_ids() {
+            println!("{}", language_id)
+        }
     }
-}
-fn update_config(ghidra: String){
+    fn update_config(ghidra: String) {
         let config: JingleConfig = confy::load("jingle", None).unwrap();
         let ghidra = PathBuf::from(ghidra);
-        if ghidra != config.ghidra_path{
-            let new_config = JingleConfig{ghidra_path: ghidra};
+        if ghidra != config.ghidra_path {
+            let new_config = JingleConfig {
+                ghidra_path: ghidra,
+            };
             confy::store("jingle", None, new_config).unwrap()
         }
     }
-    
-
 }
