62a479e37f4f79bcc98eabd1e65fabe60404fabf
Mark <mark.denhoed@cs.ox.ac.uk>
2024-10-15 11:34:54 +0100

    Some build fixes

--- jingle_sleigh/src/ffi/image.rs
+++ jingle_sleigh/src/ffi/image.rs
@@ -1,28 +1,30 @@
-use std::fmt::{Debug, Formatter};
 use crate::context::image::ImageProvider;
 use crate::ffi::instruction::bridge::VarnodeInfoFFI;
 use crate::VarNode;
+use std::fmt::{Debug, Formatter};
 
-pub(crate) struct ImageFFI{
-    provider: Box<dyn ImageProvider>
+pub(crate) struct ImageFFI {
+    provider: Box<dyn ImageProvider>,
 }
 
-impl ImageFFI{
-    pub(crate) fn new<T: ImageProvider>(provider: T) -> Self{
-        Self{provider: Box::new(provider)}
+impl ImageFFI {
+    pub(crate) fn new<T: ImageProvider>(provider: T) -> Self {
+        Self {
+            provider: Box::new(provider),
+        }
     }
-    fn load(&self, vn: &VarnodeInfoFFI, out: &mut [u8]) -> usize{
+    fn load(&self, vn: &VarnodeInfoFFI, out: &mut [u8]) -> usize {
         self.provider.load(VarNode::from(vn), out)
     }
 }
 
 #[cxx::bridge]
 pub(crate) mod bridge {
-    extern "C++"{
+    unsafe extern "C++" {
         type VarnodeInfoFFI = crate::ffi::instruction::bridge::VarnodeInfoFFI;
     }
-    extern "Rust"{
-
+    extern "Rust" {
+        include!("jingle_sleigh/src/ffi/instruction.rs.h");
         type ImageFFI;
         fn load(self: &ImageFFI, vn: &VarnodeInfoFFI, out: &mut [u8]) -> usize;
     }
