f4c79b551a6eb6d4dee723c8e0f9f725d4d364f7
Mark <mark.denhoed@cs.ox.ac.uk>
2025-03-30 18:26:59 +0100

    Read registers and RAM from a state

--- jingle_python/script.py
+++ jingle_python/script.py
@@ -3,5 +3,5 @@ from jingle import *
 sleigh = create_sleigh_context("/Users/maroed/RustroverProjects/code_reuse_synthesis_artifacts/crackers/libz.so.1", "/Applications/ghidra")
 j = sleigh.make_jingle_context()
 state = State(j)
-bv = state.read_varnode(VarNode(2, 100, 2))
-print(bv + bv)
+
+print(state.register("RAX") == state.ram(0x8000_0000, 8))
--- jingle_python/src/state.rs
+++ jingle_python/src/state.rs
@@ -1,10 +1,11 @@
 use crate::bitvec::adapt_bv;
 use crate::jingle_context::PythonJingleContext;
 use jingle::modeling::State;
-use jingle::sleigh::VarNode;
+use jingle::sleigh::{ArchInfoProvider, VarNode};
+use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
 
-#[pyclass(unsendable, name="State")]
+#[pyclass(unsendable, name = "State")]
 pub struct PythonState {
     state: State<'static>,
 }
@@ -18,7 +19,23 @@ impl PythonState {
         })
     }
 
-    pub fn read_varnode(&self, varnode: &VarNode) -> PyResult<PyObject> {
+    pub fn varnode(&self, varnode: &VarNode) -> PyResult<PyObject> {
         adapt_bv(self.state.read_varnode(varnode)?)
+    }
+
+    pub fn register(&self, name: &str) -> PyResult<PyObject> {
+        let vn = self
+            .state
+            .get_register(name)
+            .ok_or(PyRuntimeError::new_err("Queried nonexistent register"))?;
+        adapt_bv(self.state.read_varnode(vn)?)
+    }
+
+    pub fn ram(&self, offset: u64, length: usize) -> PyResult<PyObject> {
+        adapt_bv(self.state.read_varnode(&VarNode {
+            offset,
+            size: length,
+            space_index: self.state.get_code_space_idx(),
+        })?)
     }
 }
