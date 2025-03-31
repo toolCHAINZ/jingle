f433c0815b089abf152e97d1b31a784c554e6587
Mark <mark.denhoed@cs.ox.ac.uk>
2025-03-31 18:57:21 +0100

    CIIII

--- jingle_python/script.py
+++ jingle_python/script.py
@@ -1,9 +1,11 @@
 import z3
+from z3 import simplify
 from jingle import *
-sleigh = create_sleigh_context("/Users/maroed/RustroverProjects/code_reuse_synthesis_artifacts/crackers/libz.so.1", "/Applications/ghidra")
+sleigh = create_sleigh_context("/Users/denhomc1/PycharmProjects/code_reuse_synthesis_artifacts/crackers/libz.so.1", "/Applications/ghidra")
 
 
 jingle = sleigh.make_jingle_context()
 model = jingle.model_block_at(0xa860, 9)
-for output in model.get_input_bvs():
-    print(z3.simplify(output))
+print(simplify(model.final_state.register("RSP") == model.original_state.register("RSP")))
+print(simplify(model.final_state.register("RAX") == model.original_state.register("RAX")))
+print(simplify(model.final_state.register("RBX") == model.original_state.register("RBX")))
