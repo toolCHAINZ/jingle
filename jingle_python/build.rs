a065d1ab9ba52d93077160dc2cfa7e9f4eac44a7
Mark <mark.denhoed@cs.ox.ac.uk>
2025-04-01 13:55:13 +0100

    tweaks

--- jingle_python/build.rs
+++ jingle_python/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     // Run the Python script to get the venv's lib directory
-    let output = Command::new("python")
+    let output = Command::new("python3")
         .arg("find_venv_library_path.py")
         .output()
         .expect("Failed to execute Python script");
