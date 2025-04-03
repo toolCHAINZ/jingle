ec46ace915bd2a1ba25b61bc6c138e107f1fa7e2
Mark <mark.denhoed@cs.ox.ac.uk>
2025-04-03 10:57:53 +0100

    path stuff

--- jingle_python/build.rs
+++ jingle_python/build.rs
@@ -8,10 +8,12 @@ fn main() {
         .expect("Failed to execute Python script");
 
     if !output.status.success() {
-        println!("WARNING: not linking with python's z3");
+        println!("cargo:warning=Could not find python's z3, this wheel is unlikely to work.");
     } else {
         let venv_lib = String::from_utf8_lossy(&output.stdout).trim().to_string();
         let z3_python_lib = std::path::Path::new(&venv_lib).join("z3").join("lib");
+        let z3_header_path = std::path::Path::new(&venv_lib).join("z3").join("include").join("z3.h");
         println!("cargo:rustc-link-search=native={}", z3_python_lib.display());
+        println!("cargo:env=Z3_SYS_Z3_HEADER={}", z3_header_path.display());
     }
 }
