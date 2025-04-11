dba96bcd26b6f11d1f7eee111a2315a0e7e53c99
Mark <mark.denhoed@cs.ox.ac.uk>
2025-04-11 18:52:20 +0100

    Add some methods

--- jingle/src/python/resolved_varnode.rs
+++ jingle/src/python/resolved_varnode.rs
@@ -1,12 +1,29 @@
+use crate::python::z3::ast::TryIntoPythonZ3;
 use crate::varnode::display::{ResolvedIndirectVarNodeDisplay, ResolvedVarNodeDisplay};
 use jingle_sleigh::VarNodeDisplay;
-use pyo3::pyclass;
+use pyo3::{pyclass, pymethods, Py, PyAny, PyResult};
 use std::fmt::{Display, Formatter};
 
 #[derive(Clone)]
 #[pyclass(unsendable, str)]
 pub struct PythonResolvedIndirectVarNode {
     pub inner: ResolvedIndirectVarNodeDisplay<'static>,
+}
+
+#[pymethods]
+impl PythonResolvedIndirectVarNode {
+    pub fn pointer_bv(&self) -> PyResult<Py<PyAny>> {
+        let ptr = self.inner.pointer.clone();
+        ptr.try_into_python()
+    }
+
+    pub fn space_name(&self) -> &str {
+        self.inner.pointer_space_info.name.as_str()
+    }
+
+    pub fn access_size(&self) -> usize {
+        self.inner.access_size_bytes
+    }
 }
 
 impl Display for PythonResolvedIndirectVarNode {
