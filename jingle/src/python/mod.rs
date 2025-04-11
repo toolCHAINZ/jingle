ee731de73427dc722f5097b81faf5721d9122d61
toolCHAINZ <mark.denhoed@cs.ox.ac.uk>
2025-04-11 11:45:21 +0100

    Python/Rust Conversion Trait (#48)
    * Use a trait for converting z3 asts. Impl for bitvec
    
    * unused import
    
    * fmt

--- jingle/src/python/mod.rs
+++ jingle/src/python/mod.rs
@@ -1,4 +1,3 @@
-pub mod bitvec;
 pub mod instruction;
 pub mod jingle_context;
 pub mod modeled_block;
@@ -6,3 +5,4 @@ pub mod modeled_instruction;
 pub mod sleigh_context;
 pub mod state;
 pub mod varode_iterator;
+pub mod z3;
--- jingle/src/python/varode_iterator.rs
+++ jingle/src/python/varode_iterator.rs
@@ -1,5 +1,5 @@
 use crate::modeling::State;
-use crate::python::bitvec::adapt_bv;
+use crate::python::z3::ast::TryIntoPythonZ3;
 use crate::varnode::ResolvedVarnode;
 use pyo3::{pyclass, pymethods, Py, PyAny, PyRef, PyRefMut};
 
@@ -29,7 +29,7 @@ impl VarNodeIterator {
     pub fn __next__(mut slf: PyRefMut<Self>) -> Option<Py<PyAny>> {
         let vn = slf.vn.next()?;
         let vn = slf.state.read_resolved(&vn).ok()?;
-        let bv = adapt_bv(vn).ok()?;
+        let bv = vn.try_into_python().ok()?;
         Some(bv)
     }
 }
