8232ef1722ff32cfa5a1a8f7e4cfbde9a53d3b51
Mark <mark.denhoed@cs.ox.ac.uk>
2025-07-24 17:39:30 +0100

    Add basic analysis traits

---
+++ jingle/src/analysis/cpa/lattice/mod.rs
@@ -0,0 +1,26 @@
+mod simple;
+
+pub trait JoinSemiLattice: Eq + PartialOrd {
+    fn join(&mut self, other: &Self);
+    // todo: add top method?
+    // fn is_top(&self) -> bool;
+}
+
+/// This trait exists to allow defining types that are
+/// "lattice-y" without requiring that they specify a Top element.
+/// Types implementing this trait can be used to construct
+/// an actual Lattice using [super::simple_lattice::SimpleLattice]
+pub trait PartialJoinSemiLattice: Eq + PartialOrd + Sized {
+    fn partial_join(&self, other: &Self) -> Option<Self>;
+}
+
+impl<S1, S2> JoinSemiLattice for (S1, S2)
+where
+    S1: JoinSemiLattice,
+    S2: JoinSemiLattice,
+{
+    fn join(&mut self, other: &Self) {
+        self.0.join(&other.0);
+        self.1.join(&other.1);
+    }
+}
---
+++ jingle/src/analysis/pcode_store.rs
@@ -0,0 +1,37 @@
+use jingle_sleigh::context::loaded::LoadedSleighContext;
+use jingle_sleigh::{ArchInfoProvider, PcodeOperation, VarNode};
+use crate::modeling::machine::cpu::concrete::ConcretePcodeAddress;
+
+pub trait PcodeStore {
+    fn get_pcode_op_at(&self, addr: ConcretePcodeAddress) -> Option<PcodeOperation>;
+
+}
+
+pub trait EntryPoint{
+    fn get_entry(&self) -> ConcretePcodeAddress;
+}
+
+
+impl<'a> PcodeStore for LoadedSleighContext<'a>{
+
+    fn get_pcode_op_at(&self, addr: ConcretePcodeAddress) -> Option<PcodeOperation> {
+        let instr = self.instruction_at(addr.machine())?;
+        if addr.pcode() as usize == instr.ops.len() {
+            Some(PcodeOperation::Branch {
+                input: VarNode {
+                    space_index: self.get_code_space_idx(),
+                    offset: addr.machine() + instr.length as u64,
+                    size: 1,
+                },
+            })
+        } else {
+            instr.ops.get(addr.pcode() as usize).cloned()
+        }
+    }
+}
+
+impl<T: PcodeStore> PcodeStore for &T {
+    fn get_pcode_op_at(&self, addr: ConcretePcodeAddress) -> Option<PcodeOperation> {
+        (*self).get_pcode_op_at(addr)
+    }
+}
