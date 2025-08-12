d87127c4f2c8b1409fdb1ed5fa234fcd1c2df399
Mark <mark.denhoed@cs.ox.ac.uk>
2025-08-12 21:27:24 +0100

    more translate

--- jingle/src/varnode/mod.rs
+++ jingle/src/varnode/mod.rs
@@ -1,6 +1,7 @@
 use jingle_sleigh::VarNode;
 use std::hash::Hash;
 use z3::ast::BV;
+use z3::{Context, Translate};
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub struct ResolvedIndirectVarNode {
@@ -10,6 +11,16 @@ pub struct ResolvedIndirectVarNode {
     pub access_size_bytes: usize,
 }
 
+unsafe impl Translate for ResolvedIndirectVarNode {
+    fn translate(&self, dest: &Context) -> Self {
+        Self {
+            pointer_space_idx: self.pointer_space_idx.clone(),
+            pointer_location: self.pointer_location.clone(),
+            access_size_bytes: self.access_size_bytes,
+            pointer: self.pointer.translate(dest)
+        }
+    }
+}
 /// This represents a general varnode that has been evaluated in a sequence of instructions.
 /// What distinguishes this from a regular VarNode is that, in the case of indirect varnodes,
 /// the pointer value has been already evaluated
@@ -22,5 +33,14 @@ pub enum ResolvedVarnode {
 impl From<VarNode> for ResolvedVarnode {
     fn from(value: VarNode) -> Self {
         Self::Direct(value)
+    }
+}
+
+unsafe impl Translate for ResolvedVarnode {
+    fn translate(&self, dest: &Context) -> Self {
+        match self {
+            ResolvedVarnode::Direct(a) => {ResolvedVarnode::Direct(a.clone())}
+            ResolvedVarnode::Indirect(i) => {ResolvedVarnode::Indirect(i.translate(dest))}
+        }
     }
 }
