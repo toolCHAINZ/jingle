e2dd45c23f64564bec7faed4752a19d778a31e4b
Mark <mark.denhoed@cs.ox.ac.uk>
2025-09-04 14:22:56 +0100

    add leaf node

--- jingle/src/analysis/cfg.rs
+++ jingle/src/analysis/cfg.rs
@@ -3,6 +3,7 @@ use crate::modeling::machine::MachineState;
 use crate::modeling::machine::cpu::concrete::ConcretePcodeAddress;
 use jingle_sleigh::PcodeOperation;
 use petgraph::Direction;
+use petgraph::data::DataMap;
 use petgraph::prelude::{DiGraph, EdgeRef};
 use std::collections::HashMap;
 use z3::ast::{Ast, Bool};
@@ -29,25 +30,39 @@ impl PcodeCfg {
         &self.graph
     }
 
+    pub fn leaf_ndoes(&self) -> impl Iterator<Item = (ConcretePcodeAddress, PcodeOperation)> {
+        self.graph
+            .node_indices()
+            .filter(|&n| {
+                self.graph
+                    .neighbors_directed(n, Direction::Outgoing)
+                    .next()
+                    .is_none()
+            })
+            .map(|n| self.graph.node_weight(n).unwrap())
+            .cloned()
+    }
+
     pub fn build_solver(&self, jingle: JingleContext) -> Solver {
         let solver = Solver::new();
         let mut states = HashMap::new();
         for addr in self.graph.node_indices() {
-            let (addr,_) = self.graph.node_weight(addr).unwrap();
+            let (addr, _) = self.graph.node_weight(addr).unwrap();
             states.insert(addr, MachineState::fresh_for_address(&jingle, *addr));
         }
 
         for idx in self.graph.node_indices() {
             let outgoing: Vec<_> = self
                 .graph
-                .edges_directed(idx, Direction::Incoming).map(|e|e.id())
+                .edges_directed(idx, Direction::Incoming)
+                .map(|e| e.id())
                 .collect();
             let options: Vec<_> = outgoing
                 .iter()
                 .map(|edge| {
                     let (fromidx, toidx) = self.graph.edge_endpoints(*edge).unwrap();
                     let (from, op) = self.graph.node_weight(fromidx).unwrap();
-                    let (to,_) = self.graph.node_weight(toidx).unwrap();
+                    let (to, _) = self.graph.node_weight(toidx).unwrap();
                     let to_state = states.get(to).expect("From state not found");
                     let from_state = states.get(from).expect("To state not found");
                     let relation = from_state.apply(op).unwrap();
@@ -73,7 +88,7 @@ impl PcodeCfg {
         let mut states = HashMap::new();
         let mut post_states = HashMap::new();
         for idx in self.graph.node_indices() {
-            let (addr,op) = self.graph.node_weight(idx).unwrap().clone();
+            let (addr, op) = self.graph.node_weight(idx).unwrap().clone();
             let s = MachineState::fresh_for_address(&jingle, addr);
             states.insert(addr, s.clone());
             if let Some(_) = self.graph.edges_directed(idx, Direction::Outgoing).next() {
@@ -82,11 +97,13 @@ impl PcodeCfg {
             }
         }
 
-        let options: Vec<_> = self.graph.edge_indices()
+        let options: Vec<_> = self
+            .graph
+            .edge_indices()
             .map(|edge| {
                 let (fromidx, toidx) = self.graph.edge_endpoints(edge).unwrap();
                 let (from, op) = self.graph.node_weight(fromidx).unwrap();
-                let (to,_) = self.graph.node_weight(toidx).unwrap();
+                let (to, _) = self.graph.node_weight(toidx).unwrap();
 
                 let from_state = states.get(from).expect("From state not found");
                 let to_state = states.get(to).expect("To state not found");
