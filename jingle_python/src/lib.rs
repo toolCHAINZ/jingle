14b54c482a846d6b466482e7dfd2d6dd7c5b26c5
Mark <mark.denhoed@cs.ox.ac.uk>
2025-09-19 13:36:19 +0100

    fmt + clippy

--- jingle_python/src/lib.rs
+++ jingle_python/src/lib.rs
@@ -1,10 +1,10 @@
 use ::jingle::python::instruction::PythonInstruction;
 use ::jingle::python::modeled_block::PythonModeledBlock;
 use ::jingle::python::modeled_instruction::PythonModeledInstruction;
+use ::jingle::python::resolved_varnode::PythonResolvedVarNode;
 use ::jingle::python::sleigh_context::PythonLoadedSleighContext;
 use ::jingle::python::state::PythonState;
 use ::jingle::sleigh::{IndirectVarNode, PcodeOperation, VarNode};
-use ::jingle::python::resolved_varnode::PythonResolvedVarNode;
 use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
