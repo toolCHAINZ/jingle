e723fc84c70126f0e17faee4c93a6764a82c12cd
Mark <mark.denhoed@cs.ox.ac.uk>
2025-10-09 11:10:43 +0100

    print log

--- jingle/examples/unwind.rs
+++ jingle/examples/unwind.rs
@@ -7,6 +7,7 @@ use jingle_sleigh::context::image::gimli::load_with_gimli;
 use petgraph::dot::Dot;
 use std::time::Instant;
 use std::{env, fs};
+use z3::{Config, Params, with_z3_config};
 
 const FUNC_LINE: u64 = 0x100000460;
 const FUNC_BRANCH: u64 = 0x100000480;
@@ -19,12 +20,12 @@ fn main() {
     let bin_path = env::home_dir()
         .unwrap()
         .join("Documents/test_funcs/build/example");
+    z3::set_global_param("trace", "true");
     let loaded = load_with_gimli(bin_path, "/Applications/ghidra").unwrap();
 
-    let mut direct = UnwindingAnalysis::new(5);
-    let pcode_graph = direct.run(&loaded, direct.make_initial_state(FUNC_NESTED.into()));
+    let mut direct = UnwindingAnalysis::new(2);
+    let pcode_graph = direct.run(&loaded, direct.make_initial_state(0x1000004a0.into()));
     let addrs = pcode_graph.nodes().collect::<Vec<_>>();
-    let pcode_graph = pcode_graph.basic_blocks();
     for addr in addrs {
         println!("{:x}", addr.location());
     }
@@ -33,13 +34,19 @@ fn main() {
 
     fs::write("dot.dot", format!("{:x}", Dot::new(&pcode_graph.graph())));
     println!("{:x?}", leaf);
-    let solver = pcode_graph.test_build(loaded.arch_info());
-    fs::write(
-        "test.smt",
-        pcode_graph.test_build(loaded.arch_info()).to_string(),
-    );
-    let t = Instant::now();
-    dbg!(solver.check());
-    dbg!(solver.get_unsat_core());
-    println!("took {:?}", t.elapsed());
+    let arch_info = loaded.arch_info();;
+    with_z3_config(&Config::new(), || {
+        let solver = pcode_graph.test_build(arch_info);
+        let mut params = Params::new();
+        params.set_bool("trace", true);
+        solver.set_params(&params);
+        fs::write(
+            "test.smt",
+            pcode_graph.test_build(arch_info).to_string(),
+        );
+        let t = Instant::now();
+        dbg!(solver.check());
+        dbg!(solver.get_unsat_core());
+        println!("took {:?}", t.elapsed());
+    })
 }
