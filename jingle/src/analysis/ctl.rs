c68cc79b8e557353aac5c568f4c0bcdd413c531a
Mark <mark.denhoed@cs.ox.ac.uk>
2025-11-28 17:38:52 +0000

    More stuff

--- jingle/src/analysis/ctl.rs
+++ jingle/src/analysis/ctl.rs
@@ -25,6 +25,22 @@ pub enum CtlFormula<N: CfgState, D: ModelTransition<N::Model>> {
     Path(PathFormula<N, D>),
 }
 
+impl<N: CfgState,D: ModelTransition<<N as CfgState>::Model>> BitAnd for CtlFormula<N,D>{
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        CtlFormula::conjunction(&self, &rhs)
+    }
+}
+
+impl<N: CfgState,D: ModelTransition<<N as CfgState>::Model>> BitOr for CtlFormula<N,D>{
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        CtlFormula::disjunction(&self, &rhs)
+    }
+}
+
 impl<N: CfgState, D: ModelTransition<N::Model>> CtlFormula<N, D> {
     pub fn bottom() -> Self {
         CtlFormula::Bottom
@@ -40,12 +56,12 @@ impl<N: CfgState, D: ModelTransition<N::Model>> CtlFormula<N, D> {
         CtlFormula::Negation(Box::new(a.as_ref().clone()))
     }
 
-    pub fn conjunction<T: AsRef<CtlFormula<N, D>>, U: AsRef<CtlFormula<N, D>>>(a: T, b: U) -> Self {
-        CtlFormula::Conjunction(Box::new(a.as_ref().clone()), Box::new(b.as_ref().clone()))
+    pub fn conjunction<T: Borrow<CtlFormula<N, D>>, U: Borrow<CtlFormula<N, D>>>(a: T, b: U) -> Self {
+        CtlFormula::Conjunction(Box::new(a.borrow().clone()), Box::new(b.borrow().clone()))
     }
 
-    pub fn disjunction<T: AsRef<CtlFormula<N, D>>, U: AsRef<CtlFormula<N, D>>>(a: T, b: U) -> Self {
-        CtlFormula::Disjunction(Box::new(a.as_ref().clone()), Box::new(b.as_ref().clone()))
+    pub fn disjunction<T: Borrow<CtlFormula<N, D>>, U: Borrow<CtlFormula<N, D>>>(a: T, b: U) -> Self {
+        CtlFormula::Disjunction(Box::new(a.borrow().clone()), Box::new(b.borrow().clone()))
     }
 
     pub fn implies<T: AsRef<CtlFormula<N, D>>, U: AsRef<CtlFormula<N, D>>>(a: T, b: U) -> Self {
@@ -62,51 +78,44 @@ impl<N: CfgState, D: ModelTransition<N::Model>> CtlFormula<N, D> {
             operation: pf.borrow().clone(),
         })
     }
-}
 
-macro_rules! ctl {
-    (!$a:expr) => {
-        CtlFormula::negation($ctl!($a))
-    };
-    (($a:tt ^ $b:tt)) => {
-        CtlFormula::conjunction(ctl!($a), ctl!($b))
-    };
-    (($a:tt v $b:tt)) => {
-        CtlFormula::disjunction(ctl!($a), ctl!($b))
-    };
-    (($a:tt => $b:tt)) => {
-        CtlFormula::implies(ctl!($a), ctl!($b))
-    };
-    (($a:tt <=> $b:tt)) => {
-        CtlFormula::iff(ctl!($a), ctl!($b))
-    };
-    ((AX $a:tt)) => {
-        CtlFormula::path(CtlQuantifier::Universal, PathOperation::next(ctl!($a)))
-    };
-    ((EX $a:tt)) => {
-        CtlFormula::path(CtlQuantifier::Existential, PathOperation::next(ctl!($a)))
-    };
-    ((AF $a:tt)) => {
-        CtlFormula::path(CtlQuantifier::Universal, PathOperation::eventually(ctl!($a)))
-    };
-    ((EF $a:tt)) => {
-        CtlFormula::path(CtlQuantifier::Existential, PathOperation::eventually(ctl!($a)))
-    };
-    ((AG $a:tt)) => {
-        CtlFormula::path(CtlQuantifier::Universal, PathOperation::always(ctl!($a)))
-    };
-    (EG $a:tt) => {
-        CtlFormula::path(CtlQuantifier::Existential, PathOperation::always(ctl!($a)))
-    };
-    (A [ $a:tt U $b:tt ]) => {
-        CtlFormula::path(CtlQuantifier::Universal, PathOperation::until(ctl!($a), ctl!($b)))
-    };
-    (E [ $a:tt U $b:tt ]) => {
-        CtlFormula::path(CtlQuantifier::Existential, PathOperation::until(ctl!($a), ctl!($b)))
-    };
-    ($f: expr) => {
-        $f
-    };
+    pub fn AX<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Universal, PathOperation::next(a))
+    }
+
+    pub fn EX<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Existential, PathOperation::next(a))
+    }
+
+    pub fn AF<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Universal, PathOperation::eventually(a))
+    }
+
+    pub fn EF<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Existential, PathOperation::eventually(a))
+    }
+
+    pub fn AG<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Universal, PathOperation::always(a))
+    }
+
+    pub fn EG<T: Borrow<CtlFormula<N, D>>>(a: T) -> Self {
+        Self::path(CtlQuantifier::Existential, PathOperation::always(a))
+    }
+
+    pub fn AU<T: Borrow<CtlFormula<N, D>>, U: Borrow<CtlFormula<N, D>>>(a: T, b: U) -> Self {
+        Self::path(
+            CtlQuantifier::Universal,
+            PathOperation::until(a, b),
+        )
+    }
+
+    pub fn EU<T: Borrow<CtlFormula<N, D>>, U: Borrow<CtlFormula<N, D>>>(a: T, b: U) -> Self {
+        Self::path(
+            CtlQuantifier::Existential,
+            PathOperation::until(a, b),
+        )
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -119,7 +128,7 @@ impl<N: CfgState, D: ModelTransition<N::Model>> PathFormula<N, D> {
     fn rewrite(&self) -> CtlFormula<N, D> {
         match (self.quantifier, self.operation.clone()) {
             (CtlQuantifier::Universal, PathOperation::Always(phi)) => {
-                ctl!((phi.clone()) ^ (AX (AX phi)))
+                phi ^ AX(AG(phi))
             }
             _ => todo!(),
         }
@@ -139,16 +148,16 @@ impl<N: CfgState, D: ModelTransition<N::Model>> PathOperation<N, D> {
         PathOperation::Next(Box::new(f.borrow().clone()))
     }
 
-    pub fn eventually<T: AsRef<CtlFormula<N, D>>>(f: T) -> Self {
-        PathOperation::Eventually(Box::new(f.as_ref().clone()))
+    pub fn eventually<T: Borrow<CtlFormula<N, D>>>(f: T) -> Self {
+        PathOperation::Eventually(Box::new(f.borrow().clone()))
     }
 
-    pub fn always<T: AsRef<CtlFormula<N, D>>>(f: T) -> Self {
-        PathOperation::Always(Box::new(f.as_ref().clone()))
+    pub fn always<T: Borrow<CtlFormula<N, D>>>(f: T) -> Self {
+        PathOperation::Always(Box::new(f.borrow().clone()))
     }
 
-    pub fn until<T: AsRef<CtlFormula<N, D>>, U: AsRef<CtlFormula<N, D>>>(a: T, b: U) -> Self {
-        PathOperation::Until(Box::new(a.as_ref().clone()), Box::new(b.as_ref().clone()))
+    pub fn until<T: Borrow<CtlFormula<N, D>>, U: Borrow<CtlFormula<N, D>>>(a: T, b: U) -> Self {
+        PathOperation::Until(Box::new(a.borrow().clone()), Box::new(b.borrow().clone()))
     }
 }
 
