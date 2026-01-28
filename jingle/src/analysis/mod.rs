fd9fddb6ea2a19ca9d7643c5498c86081fe38b33
Mark <mark.denhoed@cs.ox.ac.uk>
2026-01-28 22:28:17 +0000

    Rename direct_valuation module to valuation

--- jingle/src/analysis/mod.rs
+++ jingle/src/analysis/mod.rs
@@ -11,11 +11,11 @@ pub mod cfg;
 pub mod compound;
 pub mod cpa;
 pub mod ctl;
-pub mod direct_valuation;
 pub mod location;
 #[expect(unused)]
 mod path;
 pub mod pcode_store;
+pub mod valuation;
 pub mod varnode;
 pub mod varnode_map;
 
