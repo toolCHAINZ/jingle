8157fc447d41d2ece61d512ac87a5e4fec2d3239
Mark <mark.denhoed@cs.ox.ac.uk>
2026-02-13 10:13:41 +0000

    fix: track unknown outputs as Top
    Comment out incorrect IntOr/BoolOr logic, track unknown outputs as Top,
    and add a debug print for Top merges in JoinSemiLattice implementation.

--- jingle/src/analysis/valuation/simple/mod.rs
+++ jingle/src/analysis/valuation/simple/mod.rs
@@ -184,16 +184,18 @@ impl SimpleValuationState {
                 }
             }
 
-            PcodeOperation::IntOr { input0, input1, .. }
-            | PcodeOperation::BoolOr { input0, input1, .. } => {
-                let a = SimpleValue::from_varnode_or_entry(self, input0);
-                let b = SimpleValue::from_varnode_or_entry(self, input1);
-                if let Some(GeneralizedVarNode::Direct(output_vn)) = op.output() {
-                    new_state
-                        .valuation
-                        .add(output_vn, SimpleValue::or(a, b).simplify());
-                }
-            }
+            // This uses the wrong "Or". When we re-add bitor, use that
+            //
+            // PcodeOperation::IntOr { input0, input1, .. }
+            // | PcodeOperation::BoolOr { input0, input1, .. } => {
+            //     let a = SimpleValue::from_varnode_or_entry(self, input0);
+            //     let b = SimpleValue::from_varnode_or_entry(self, input1);
+            //     if let Some(GeneralizedVarNode::Direct(output_vn)) = op.output() {
+            //         new_state
+            //             .valuation
+            //             .add(output_vn, SimpleValue::or(a, b).simplify());
+            //     }
+            // }
 
             // Approximate shifts as addition (conservative)
             PcodeOperation::IntLeftShift { input0, input1, .. }
@@ -239,14 +241,21 @@ impl SimpleValuationState {
             }
 
             PcodeOperation::IntSExt { input, .. } | PcodeOperation::IntZExt { input, .. } => {
+                // todo, just have a self.read that takes a gneral varnode and does
+                // either a direct or indirect read?
                 let v = SimpleValue::from_varnode_or_entry(self, input);
                 if let Some(GeneralizedVarNode::Direct(output_vn)) = op.output() {
                     new_state.valuation.add(output_vn, v.simplify());
                 }
             }
 
-            // Other operations we don't model produce no tracked writes here.
-            _ => {}
+            // Other operations we don't model produce writes of Top.
+            _ => {
+                if let Some(GeneralizedVarNode::Direct(vn)) = op.output() {
+                    // todo handle indirect
+                    new_state.valuation.add(vn, SimpleValue::Top);
+                }
+            }
         }
 
         // Clear internal-space varnodes on control-flow to non-const destinations (same policy as direct_valuation.rs)
