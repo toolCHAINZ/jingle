0e65600abc0f581ed9d7bfacfacc8d56655dbb63
Mark <mark.denhoed@cs.ox.ac.uk>
2026-04-30 21:18:09 +0100

    fix

--- jingle/src/python/resolved_varnode.rs
+++ jingle/src/python/resolved_varnode.rs
@@ -1,6 +1,6 @@
 use crate::varnode::{ResolvedIndirectVarNode, ResolvedVarnode};
-use jingle_sleigh::{JingleDisplay, SleighArchInfo, VarNode};
 use jingle_sleigh::display::JingleDisplayWrapper;
+use jingle_sleigh::{JingleDisplay, SleighArchInfo, VarNode};
 use pyo3::pyclass;
 use std::fmt::{Display, Formatter};
 
@@ -18,12 +18,15 @@ pub struct PythonResolvedVarNode {
 
 impl Display for PythonResolvedVarNode {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        self.inner.fmt_jingle(f,&self.info)
+        self.inner.fmt_jingle(f, &self.info)
     }
 }
 
-impl From<JingleDisplayWrapper<ResolvedVarnode>> for PythonResolvedVarNode {
+impl From<JingleDisplayWrapper<'_, ResolvedVarnode>> for PythonResolvedVarNode {
     fn from(value: JingleDisplayWrapper<ResolvedVarnode>) -> Self {
-        Self { inner: value.inner().clone(), info: value.info().clone() }
+        Self {
+            inner: value.inner().clone(),
+            info: value.info().clone(),
+        }
     }
 }
